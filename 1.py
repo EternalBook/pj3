f359d9afab07b69c1febde4e1eb40c19d070db00
EternalBook <2435320866@qq.com>
2023-04-17 16:25:25 +0800

    Initial commit

--- 1.py
+++ 1.py
@@ -7,9 +7,12 @@ def data_import():
 
 
 def item_import():
-    lis = [['movie_id', 'movie_name', 'time', 'unix-imdb_url'], list(range(5, 25))]
-    item_csv = pd.read_csv(r'./data/u.item', sep="|", encoding="ISO-8859-1", names=lis[0]+lis[1])
-
+    # 读取csv文件并为前四列设置一级标题，后20列设置二级标题
+    item_csv = pd.read_csv('./data/u.item', sep='|', encoding='ISO-8859-1',
+                           names=['movie_id', 'title', 'release_date', 'url'] + [i for i in range(20)],
+                           usecols=list(range(24)))
+    # 为前四列设置二级标题
+    # item_csv.columns = pd.MultiIndex.from_product([['Movie_Info'], item_csv.columns])
     return item_csv
 
 
@@ -20,5 +23,6 @@ def user_import():
 
 if __name__ == "__main__":
     # print(data_import())
+    pd.set_option('display.max_columns', None)
     print(item_import())
     # print(user_import())
